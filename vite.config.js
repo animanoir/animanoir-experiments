9f2d91147f660afdb6d591eaad9d74de77a06c3a
oscaramontiel <etherealflux94@gmail.com>
2025-04-09 16:52:36 -0600

    Update Vite configuration to include new LED experiment entry point

--- vite.config.js
+++ vite.config.js
@@ -15,8 +15,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        menu: resolve(__dirname, 'src/experiments/index.html'),
         tjsBuddha: resolve(__dirname, 'src/experiments/acidBuddha/index.html'),
+        led: resolve(__dirname, 'src/experiments/led/index.html'),
       }
     }
   }
