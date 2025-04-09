640322f586285ec6c65abb87a89554750443783e
oscaramontiel <etherealflux94@gmail.com>
2025-04-09 11:32:11 -0600

    Update Vite configuration to include additional entry points for experiments

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
 export default {
   root: 'src/',
   publicDir: '../static',
-  base: "/",
+  base: '/', // Ensure proper routing for Netlify
   server: {
     host: true, // Open to local network and display URL
   },
-  build:{
+  build: {
     outDir: '../dist',
     emptyOutDir: true, // Empty the folder first.
-    sourcemap: 'hidden' // Add sourcemap
+    sourcemap: 'hidden', // Add sourcemap
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+        menu: 'src/experiments/menu.html',
+        threejs: 'src/experiments/threejs/index.html',
+        v0: 'src/experiments/v0/v0.html',
+        vr1: 'src/experiments/vr1/index.html',
+        webAntigua: 'src/experiments/webAntigua/404.html' // Add other entry points as needed
+      }
+    }
   }
 }
 
-// A source map is a file that creates a mapping between your original source code and the minified/transformed code that runs in production. 
+// A source map is a file that creates a mapping between your original source code and the minified/transformed code that runs in production.
