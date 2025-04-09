262cfe92c2732686bf46ff3ed19c7511918ff979
oscaramontiel <etherealflux94@gmail.com>
2025-04-09 12:24:53 -0600

    Fix input path for menu experiment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        menu: resolve(__dirname, 'src/experiments/menu.html'),
+        menu: resolve(__dirname, 'src/experiments/index.html'),
         threejs: resolve(__dirname, 'src/experiments/threejs/index.html'),
         v0: resolve(__dirname, 'src/experiments/v0/v0.html'),
         vr1: resolve(__dirname, 'src/experiments/vr1/index.html'),
